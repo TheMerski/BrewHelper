3b4bdd349a3a5d92bbfa81ee36d69e8cee131656
Merwin Hill <merwin@quicknet.nl>
2021-02-11 21:36:58 +0100

    Added Recipe Model

---
+++ BrewHelper/BrewHelper/Models/Recipe.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrewHelper.Models
+{
+    public class Recipe
+    {
+        public long Id { get; set; }
+        /// <summary>
+        /// Recipe name
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Recipe Description
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// Expected start SG
+        /// </summary>
+        public int StartSG { get; set; }
+        /// <summary>
+        /// Expected end SG
+        /// </summary>
+        public int EndSG { get; set; }
+        /// <summary>
+        /// Expected yield (in L)
+        /// </summary>
+        public int Yield { get; set; }
+        /// <summary>
+        /// Expected days before beer is ready
+        /// </summary>
+        public int ReadyAfter { get; set; }
+        /// <summary>
+        /// Expected alcohol percentage
+        /// </summary>
+        public double AlcoholPercentage { get; set; }
+        /// <summary>
+        /// Expected IBU (bitterness)
+        /// </summary>
+        public double IBU { get; set; }
+        /// <summary>
+        /// Expected EBC
+        /// </summary>
+        public double EBC { get; set; }
+
+        /// <summary>
+        /// Mashwater to use (in L)
+        /// </summary>
+        public double MashWater { get; set; }
+        /// <summary>
+        /// Mash ingredients (weight in grams)
+        /// </summary>
+        public Dictionary<Ingredient, int> MashIngredients { get; set; }
+        /// <summary>
+        /// Steps for mashing phase
+        /// </summary>
+        public List<RecipeStep> MashSteps { get; set; }
+        /// <summary>
+        /// Rinse water before boiling
+        /// </summary>
+        public double RinseWater { get; set; }
+        /// <summary>
+        /// Steps for boiling
+        /// </summary>
+        public List<RecipeStep> BoilingSteps { get; set; }
+        /// <summary>
+        /// Steps for yeasting
+        /// </summary>
+        public List<RecipeStep> YeastingSteps { get; set; }
+    }
+
+    public class RecipeStep
+    {
+        public long Id { get; set; }
+        /// <summary>
+        /// Step name
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Step Description
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// Amount of time for step
+        /// </summary>
+        public int Time { get; set; }
+        /// <summary>
+        /// Temperature for step (if applicable)
+        /// </summary>
+        public double? Temperature { get; set; }
+        /// <summary>
+        /// Ingredients needed for step (weight in grams)
+        /// </summary>
+        public Dictionary<Ingredient, int> Ingredients { get; set; }
+    }
+}
--- BrewHelper/BrewHelper/Models/RecipeContext.cs
+++ BrewHelper/BrewHelper/Models/RecipeContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,39 @@ namespace BrewHelper.Models
         }
         
         public DbSet<Ingredient> Ingredients { get; set; }
+
+        public DbSet<Recipe> Recipes { get; set; }
+
+        public DbSet<RecipeStep> RecipeSteps { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Recipe>(b =>
+            {
+                b.Property(r => r.Name).IsRequired();
+
+                b.Property(r => r.MashIngredients)
+                    .HasConversion(
+                        d => JsonConvert.SerializeObject(d, Formatting.None),
+                        s => JsonConvert.DeserializeObject<Dictionary<Ingredient, int>>(s)
+                    )
+                    .HasMaxLength(4000);
+            });
+
+            modelBuilder.Entity<RecipeStep>(b =>
+            {
+                b.Property(r => r.Name).IsRequired();
+
+                b.Property(r => r.Ingredients)
+                    .HasConversion(
+                        d => JsonConvert.SerializeObject(d, Formatting.None),
+                        s => JsonConvert.DeserializeObject<Dictionary<Ingredient, int>>(s)
+                    )
+                    .HasMaxLength(4000);
+            });
+        }
     }
 }
