33b8104d7d5fd5f907371866b1e36c563a6e4331
Merwin Hill <merwin@quicknet.nl>
2021-02-11 20:56:54 +0100

    Created Initial BrewHelper solution & api

---
+++ BrewHelper/BrewHelper/Models/RecipeContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrewHelper.Models
+{
+    public class RecipeContext : DbContext
+    {
+        public RecipeContext(DbContextOptions<RecipeContext> options): base(options)
+        {
+        }
+        
+        public DbSet<Ingredient> Ingredients { get; set; }
+    }
+}
