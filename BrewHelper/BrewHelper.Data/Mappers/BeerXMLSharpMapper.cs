f4e35aa3041702101545704457c5bdb5bf3f5738
Merwin Hill <merwin@quicknet.nl>
2022-04-06 23:04:34 +0200

    Pasting XML as class is class

---
+++ BrewHelper/BrewHelper.Data/Mappers/BeerXMLSharpMapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrewHelper.Data.Entities;
+
+namespace BrewHelper.Data.Mappers;
+public static class BeerXMLSharpMapper
+{
+    public static IEnumerable<Recipe> ToRecipeEnumerator(this RECIPES recipes)
+    {
+        return recipes.RECIPE.Select(r => r.ToRecipe()).AsEnumerable();
+    }
+
+    public static Recipe ToRecipe(this RECIPESRECIPE recipe)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ BrewHelper/BrewHelper.Data/Mappers/BeerXMLmapper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using BrewHelper.Data.Entities;
+using BrewHelper.Data.Exceptions;
+
+namespace BrewHelper.Data.Mappers;
+internal class BeerXMLmapper : IBeerXMLMapper
+{
+    public BeerXMLmapper()
+    {
+    }
+
+    public Task<IEnumerable<Recipe>> MapRecipes(string xml)
+    {
+        byte[] byteArray = Encoding.ASCII.GetBytes(xml);
+        MemoryStream stream = new MemoryStream(byteArray);
+        return this.MapRecipes(stream);
+    }
+
+    public Task<IEnumerable<Recipe>> MapRecipes(Stream xml)
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(RECIPES));
+        RECIPES? recipes;
+        recipes = (RECIPES?)serializer.Deserialize(xml);
+        if (recipes == null)
+        {
+            throw new Exception("Something went wrong deserializing");
+        }
+        else
+        {
+            IEnumerable<Recipe> enumerator = recipes.ToRecipeEnumerator();
+            return Task.FromResult(enumerator);
+        }
+    }
+}
