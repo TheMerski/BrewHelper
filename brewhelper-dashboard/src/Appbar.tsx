d92568585de77baef3c459e52e48e0d05737b417
Merwin Hill <merwin@q42.nl>
2022-05-09 10:53:37 +0200

    Removed react-admin dashboard in favor of blazor + Removed docker-compose files since they are not up to date

--- brewhelper-dashboard/src/Appbar.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { AppBar } from 'react-admin';
-import MyUserMenu from './Menu';
-
-function MyAppBar(props: any) {
-  return <AppBar {...props} userMenu={<MyUserMenu />} />;
-}
-export default MyAppBar;
--- brewhelper-dashboard/src/Layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Layout } from 'react-admin';
-import MyAppBar from './Appbar';
-
-function MyLayout(props: any) {
-  return <Layout {...props} appBar={MyAppBar} />;
-}
-
-export default MyLayout;
